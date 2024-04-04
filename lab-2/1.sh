cddbbca16352672cf8babbf58b6fbdd01aedc6a0
Grigoriy <113019813+timodotodot@users.noreply.github.com>
2024-04-04 18:45:23 +0300

    fix: lab-2/1.sh

--- lab-2/1.sh
+++ lab-2/1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ps -U root --no-headers | wc -l > 1.log
-ps -U root -o pid,command | sed -r "s/\s*([0-9]+)\s(.+)$/\1:\2/" | >> 1.log
+ps -U root -o pid,command | sed -r "s/\s*([0-9]+)\s(.+)$/\1:\2/" >> 1.log
